c54fe2750abefe2d5ef0174cc2a5d3c44961cfdf
12malak <dania.a.altarifi@gmail.com>
2024-09-05 09:23:03 +0300

    pull

--- src/App.js
+++ src/App.js
@@ -4,24 +4,10 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Pages/Home';
 import Navbar from './components/Navbar';
 import "./App.css";
-import Home from "./Pages/Home";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
 import Abuot from './Pages/Abuot';
 function App() {
   return (
-    <div className="App">
-      <Router>
-      <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/Abuot" element={<Abuot />} />
-
-        </Routes>
-      </Router>
-    
-    
-    </div>
+  
     <Router>
       <div className="App">
         <Routes>
