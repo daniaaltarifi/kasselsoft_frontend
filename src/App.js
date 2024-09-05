8e9586717439e284ef563aebd72927b5d8920eb5
12malak <dania.a.altarifi@gmail.com>
2024-09-05 18:19:30 +0300

    footer

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React,{useContext,useEffect,useState} from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Pages/Home';
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 import "./App.css";
 
 import About from './Pages/About';
@@ -17,6 +18,7 @@ function App() {
           <Route path="/About" element={<About />} />
         </Routes>
       </div>
+      <Footer />
     </Router>
   );
 }
